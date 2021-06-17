7eb1c50d8962b0f479861f153314f9d4a5d826c6
Alger Makiputin <algerzxc@gmail.com>
2021-06-17 10:28:45 +0800

    facebook community link

--- resources/js/templates/Header.js
+++ resources/js/templates/Header.js
@@ -3,9 +3,24 @@ import { Fragment } from "react";
 import logo from '../../../public/images/logo.png'
 import UserContext from '../UserContext'
 import { Link} from 'react-router-dom'
+import { Button, Modal } from "react-bootstrap"
 
 class Header extends React.Component { 
 
+    constructor(props){
+        super(props)
+        this.state = {
+            show:false
+        }
+    }
+    handleClose() {
+
+        this.setState({show:false})
+    }
+
+    handleOpen() {
+        this.setState({show:true})
+    }
     render() {
 
         return ( 
@@ -21,10 +36,12 @@ class Header extends React.Component {
                                 <img src={logo} width={180} /> 
                             </b>
                         </Link> 
-                    </div> 
-                    
+                    </div>  
                     <div style={{width:"100%",paddingRight:'20px'}}>
                         <ul className="navbar-nav float-end"> 
+                            <li className="nav-item">
+                                <button style={{marginTop:'14px'}} onClick={() => this.handleOpen()} className="btn btn-primary"><i className="mdi mdi-facebook"></i> Join our Community</button>&nbsp;
+                            </li>
                             <li className="nav-item dropdown"> 
                                 <div className="dropdown ">
                                     <UserContext.Consumer>
@@ -46,6 +63,22 @@ class Header extends React.Component {
                         </ul>
                     </div>
                 </nav>
+                <Modal show={this.state.show} onHide={() => this.handleClose()}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Join our Facebook Community</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p style={{fontWeight:'normal'}}>Woohoo, Hero Journals just recently launch. We love to hear your opinions and suggestions on how we can improve Hero Journals. Join our Facebook community now and start interacting with us!</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                    <a href="https://www.facebook.com/groups/4085551641521353" target="__blank" className="btn btn-primary" style={{color:'#fff'}}>
+                        <i className="mdi mdi-link"></i> Join Now
+                    </a>
+                    <Button variant="secondary" onClick={() => this.handleClose()}>
+                        Close
+                    </Button> 
+                    </Modal.Footer>
+                </Modal>
             </header>
         )
     }
